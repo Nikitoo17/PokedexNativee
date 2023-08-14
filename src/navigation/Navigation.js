d9b0577cbf9b54e2518dd5f9e23e16f017307597
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-14 17:29:40 -0300

    first

---
+++ src/navigation/Navigation.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import { Image } from "react-native";
+import FavoriteNavigation from "./FavoriteNavigation";
+import PokedexNavigation from "./PokedexNavigation";
+import AccountNavigation from "./AccountNavigation";
+
+const Tab = createBottomTabNavigator();
+
+export default function navigation() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Favorite"
+        component={FavoriteNavigation}
+        options={{
+          tabBarLabel: "Favoritos",
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="heart" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Pokedex"
+        component={PokedexNavigation}
+        options={{
+          tabBarLabel: "",
+          tabBarIcon: () => renderPokeball(),
+        }}
+      />
+      <Tab.Screen
+        name="Account"
+        component={AccountNavigation}
+        options={{
+          tabBarLabel: "Mi Cuenta",
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="user" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+function renderPokeball() {
+  return (
+    <Image
+      source={require("../assets/pokeball.png")}
+      style={{ width: 40, height: 40, top: 0 }}
+    />
+  );
+}
---
+++ src/screens/Pokedex.js
@@ -0,0 +1,43 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Pokedex() {
+  return (
+    <SafeAreaView style={{ justifyContent: "space-between", height: 400 }}>
+      <Text
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "red",
+          textAlign: "center",
+          color: "white",
+        }}
+      >
+        Pokedex
+      </Text>
+      <Text
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "red",
+          textAlign: "center",
+          color: "white",
+        }}
+      >
+        Pokedex
+      </Text>
+      <Text
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "red",
+          textAlign: "center",
+          color: "white",
+        }}
+      >
+        Pokedex
+      </Text>
+    </SafeAreaView>
+  );
+}
