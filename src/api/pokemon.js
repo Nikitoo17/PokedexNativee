7324b1c9e502e5ab74f18c51a8fac1ff090adab2
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-15 16:35:22 -0300

    Create pokemon.js

---
+++ src/api/pokemon.js
@@ -0,0 +1,8 @@
+import { API_POKE } from "../utils/constants";
+export const getPokemonApi = async () =>{
+    try {
+        const url =`${API_POKE}/pokemon?limit=20&offset=0`
+    } catch () {
+        
+    }
+}
