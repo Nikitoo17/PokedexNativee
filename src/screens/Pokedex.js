409b6fcb054fb1c759e43ced4327a54624a08b06
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-15 17:00:16 -0300

    Update Pokedex.js

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,7 +1,7 @@
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import React, { useState, useEffect } from "react";
-import { getPokemonApi } from "../api/pokemon";
+import { getPokemonApi, getPokemonDetailByUrlApi } from "../api/pokemon";
 
 export default function Pokedex() {
   useEffect(() => {
