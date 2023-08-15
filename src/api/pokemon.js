fd87bf86ead89e97d288fcb5db09559f04e958fc
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-15 16:39:47 -0300

    Update pokemon.js

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -1,7 +1,11 @@
+import axios from "axios";
 import { API_POKE } from "../utils/constants";
 export const getPokemonApi = async () => {
   try {
     const url = `${API_POKE}/pokemon?limit=20&offset=0`;
+    const response = await axios.get(url);
+    const result = await response.json();
+    return result;
   } catch (error) {
     throw error;
   }
