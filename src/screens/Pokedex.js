f9a35e1d34a98948284f4b628362563c74b02554
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-16 15:38:18 -0300

    Update Pokedex.js

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,8 +1,7 @@
-import { View, Text, ScrollView, Image } from "react-native";
+import { View, Text, ScrollView, Image, Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import React, { useState, useEffect } from "react";
 import { getPokemonApi, getPokemonDetailsByUrlApi } from "../api/pokemon";
-import { shouldUseActivityState } from "react-native-screens";
 
 export default function Pokedex() {
   const [pokemons, setPokemons] = useState([]);
@@ -37,88 +36,101 @@ export default function Pokedex() {
     }
   };
 
-  console.log(pokemons);
   return (
-    <ScrollView
-      style={{
-        backgroundColor: "white",
-        width: "100%",
-      }}
-      contentContainerStyle={{
-        alignItems: "center",
-      }}
-    >
-      {pokemons &&
-        pokemons.map((pokemon, index) => (
-          <View
-            key={index}
-            style={{
-              marginBottom: 10,
-              alignItems: "center",
-              borderWidth: 1,
-              borderColor: "purple",
-              borderRadius: 20,
-              height: 300,
-              width: 300,
-              gap: 1,
-              backgroundColor:
-                pokemon.type === "grass"
-                  ? "rgba(0, 255, 0, 0.5)"
-                  : pokemon.type === "fire"
-                  ? "rgba(255, 0, 0, 0.5)"
-                  : pokemon.type === "water"
-                  ? "rgba(0, 0, 255, 0.5)"
-                  : pokemon.type === "bug"
-                  ? "rgba(0,255, 0, 1)"
-                  : pokemon.type === "normal"
-                  ? "rgb(200, 200, 200)"
-                  : "white",
-            }}
-          >
-            <Image
-              source={{ uri: pokemon.imagen }}
-              style={{ width: 200, height: 200 }}
-            />
-            <Text
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView
+        style={{
+          backgroundColor: "white",
+          width: "100%",
+        }}
+        contentContainerStyle={{
+          alignItems: "center",
+        }}
+      >
+        {pokemons &&
+          pokemons.map((pokemon, index) => (
+            <View
+              key={index}
               style={{
-                color: "white",
-                backgroundColor: "black",
-                fontSize: 25,
+                marginBottom: 10,
+                alignItems: "center",
+                borderWidth: 1,
+                borderColor: "purple",
                 borderRadius: 20,
-                paddingHorizontal: 10,
+                height: 300,
+                width: 300,
+                gap: 1,
+                backgroundColor:
+                  pokemon.type === "grass"
+                    ? "rgba(0, 255, 0, 0.5)"
+                    : pokemon.type === "fire"
+                    ? "rgba(255, 0, 0, 0.5)"
+                    : pokemon.type === "water"
+                    ? "rgba(0, 0, 255, 0.5)"
+                    : pokemon.type === "bug"
+                    ? "rgba(0,255, 0, 1)"
+                    : pokemon.type === "normal"
+                    ? "rgb(200, 200, 200)"
+                    : "white",
               }}
             >
-              {pokemon.name}
-            </Text>
-            <Text
-              style={{
-                color: "white",
-                backgroundColor: "black",
-                position: "absolute",
-                top: -10,
-                right: -10,
-                fontSize: 25,
-                borderRadius: 99,
-                width: 40,
-                height: 40,
-                textAlign: "center",
-              }}
-            >
-              {pokemon.order}
-            </Text>
-            <Text
-              style={{
-                color: "white",
-                backgroundColor: "black",
-                fontSize: 18,
-                borderRadius: 20,
-                paddingHorizontal: 10,
-              }}
-            >
-              {pokemon.type}
-            </Text>
-          </View>
-        ))}
-    </ScrollView>
+              <Image
+                source={{ uri: pokemon.imagen }}
+                style={{ width: 200, height: 200 }}
+              />
+              <Text
+                style={{
+                  color: "white",
+                  backgroundColor: "black",
+                  fontSize: 25,
+                  borderRadius: 20,
+                  paddingHorizontal: 10,
+                }}
+              >
+                {pokemon.name}
+              </Text>
+              <Text
+                style={{
+                  color: "white",
+                  backgroundColor: "black",
+                  position: "absolute",
+                  top: -10,
+                  right: -10,
+                  fontSize: 25,
+                  borderRadius: 99,
+                  width: 40,
+                  height: 40,
+                  textAlign: "center",
+                }}
+              >
+                {pokemon.order}
+              </Text>
+              <Text
+                style={{
+                  color: "white",
+                  backgroundColor: "black",
+                  fontSize: 18,
+                  borderRadius: 20,
+                  paddingHorizontal: 10,
+                }}
+              >
+                {pokemon.type}
+              </Text>
+            </View>
+          ))}
+      </ScrollView>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          width: "100%",
+          justifyContent: "space-between",
+        }}
+      >
+        <Button title="Anterior" />
+        <Text>1</Text>
+        <Button title="Siguiente" />
+      </View>
+    </SafeAreaView>
   );
 }
