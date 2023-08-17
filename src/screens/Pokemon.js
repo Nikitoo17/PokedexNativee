f2cee1f55c9409acc4e9f3205ade2cb09d991cb2
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-17 18:18:53 -0300

    Create Pokemon.js

---
+++ src/screens/Pokemon.js
@@ -0,0 +1,28 @@
+import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { getPokemonDetailsApi } from "../api/pokemon";
+
+export default function Pokemon(props) {
+  const {
+    navigation,
+    route: { params },
+  } = props;
+
+  const [pokemon, setPokemon] = useState(null);
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await getPokemonDetailsApi(params.id);
+        setPokemon(response);
+      } catch (error) {
+        navigation.goBack();
+      }
+    })();
+  }, [params]);
+
+  return (
+    <View>
+      <Text>Pokemon</Text>
+    </View>
+  );
+}
