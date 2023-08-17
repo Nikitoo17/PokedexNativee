839cad394d932c0a39c297813ecf33c702b02fa2
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-17 18:18:48 -0300

    Update PokemonList.js

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,14 +1,10 @@
-import {
-  ActivityIndicator,
-  FlatList,
-  StyleSheet,
-  Platform,
-} from "react-native";
+import { View, ActivityIndicator, FlatList, StyleSheet } from "react-native";
 import React from "react";
 import PokemonCard from "./PokemonCard";
+import { Platform } from "react-native";
 
 export default function PokemonList(props) {
-  const { pokemons, loadPokemons, nextUrl } = props;
+  const { pokemons, loadPokemons, isNext } = props;
 
   const loadMore = () => {
     loadPokemons();
@@ -16,18 +12,21 @@ export default function PokemonList(props) {
 
   return (
     <FlatList
-      styles={{ height: "100%", width: "100%" }}
       data={pokemons}
       numColumns={2}
-      showsVerticalScrollIndicator={false} // Para que no aparezca la barra de scroll
+      showsVerticalScrollIndicator={false}
       keyExtractor={(pokemon) => String(pokemon.id)}
       renderItem={({ item }) => <PokemonCard pokemon={item} />}
       contentContainerStyle={styles.flatListContentContainer}
-      onEndReached={nextUrl && loadMore}
+      onEndReached={isNext && loadMore}
       onEndReachedThreshold={0.1}
       ListFooterComponent={
-        nextUrl && (
-          <ActivityIndicator size="large" style={styles.spinner} color="red" />
+        isNext && (
+          <ActivityIndicator
+            size="large"
+            style={styles.spinner}
+            color="#AEAEAE"
+          />
         )
       }
     />
@@ -37,10 +36,10 @@ export default function PokemonList(props) {
 const styles = StyleSheet.create({
   flatListContentContainer: {
     paddingHorizontal: 5,
-    fontSize: 16,
+    marginTop: Platform.OS === "android" ? 30 : 0,
   },
   spinner: {
     marginTop: 20,
-    marginBottom: 60,
+    marginBottom: Platform.OS === "android" ? 90 : 60,
   },
 });
