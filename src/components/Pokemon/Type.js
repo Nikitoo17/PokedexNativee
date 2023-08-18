ef3e57782e4bb0890d986e78e6a104bfb169aa54
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-18 16:43:16 -0300

    Create Type.js

---
+++ src/components/Pokemon/Type.js
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+export default function Type(props) {
+  const { types } = props;
+  return (
+    <View>
+      <Text>Type</Text>
+    </View>
+  );
+}
