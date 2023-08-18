80ac861d6580c00ed01bf66d1d745853072d59a7
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-18 16:44:39 -0300

    Update Type.js

--- src/components/Pokemon/Type.js
+++ src/components/Pokemon/Type.js
@@ -1,11 +1,19 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
+import { map, capitalize } from "lodash";
 
 export default function Type(props) {
   const { types } = props;
   return (
-    <View>
+    <View style={styles.content}>
       <Text>Type</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  content: {
+    paddingHorizontal: 20,
+    marginTop: 40,
+  },
+});
