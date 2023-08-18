736d96f6251799028872c29c094b5dd7bbb80728
Nikitoo17 <nicolasfadel17@gmail.com>
2023-08-18 17:12:16 -0300

    Create Stats.js

---
+++ src/components/Pokemon/Stats.js
@@ -0,0 +1,11 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+export default function Stats(props) {
+  const { stats } = props;
+  return (
+    <View>
+      <Text>Stats</Text>
+    </View>
+  );
+}
